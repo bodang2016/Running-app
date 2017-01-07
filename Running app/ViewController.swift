b046c6ebc43cf849f6d502159a2f403e79b8427e
bodang <bodang@qq.com>
2017-01-07 14:27:25 +0000

    Initial Commit

---
+++ Running app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Running app
+//
+//  Created by Bodang on 07/01/2017.
+//  Copyright © 2017 Bodang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
